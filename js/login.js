c59314c2ed91564b5fadf5e930c83667b725071c
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-17 00:28:35 +0100

    Login Alert info for page reload after redirection

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
 var loginAttempts = 0;
 
 window.onload = function() {
-    alert("!!! Development Stage !!!\n\nAdmin-Login:\n  Name: Admin\n  Password: Admin\n\nStaff-Login:\n  Name: any\n  Password: any")
+    alert("!!! Development Stage !!!\n\nAdmin-Login:\n  Name: Admin\n  Password: Admin\n\nStaff-Login:\n  Name: any\n  Password: any \n\nSince there is no Back-End yet, you will have to reload the html Page after redirection")
 }
 
 function validateAdminLogin() {
