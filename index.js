ecbe8d95796d8bc42904261dfc7d5dd4e187a8b5
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-18 14:12:32 +0100

    Node.js Log

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  console.log(res)
+  console.log(res.url)
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
