fe979e076ab4698055eae57930f9308ff2448afd
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-18 14:50:12 +0100

    Port Listener

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
