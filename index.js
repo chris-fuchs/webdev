c3547a543c3f2158eadeb615f3b08f1ffe8a1ca0
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-18 14:30:17 +0100

    Express in index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const http = require('http');
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+// const http = require('http');
+// const PORT = process.env.PORT || 5000;
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
   
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+// server.listen(PORT, () => {
+//   console.log(`Server running on ${PORT}/`);
+// });
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
