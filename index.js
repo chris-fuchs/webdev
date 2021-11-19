ad84dfe8f668011bc1d924dc609e5dfc0b6c69b9
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-19 12:15:49 +0100

    fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
-router.get('/',function(req,res){
+app.get('/',function(req,res){
     let indexPath = path.join(__dirname+'/index.html');
     console.log(indexPath);
     //res.sendFile("index.html");
