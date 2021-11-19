c73495572d9ca6acb54cc79082952a6480d635d0
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-19 12:24:22 +0100

    way to load css

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
+  // https://stackoverflow.com/a/38757303
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname));
+
 app.get('/',function(req,res){
     let indexPath = path.join(__dirname+'/index.html');
     console.log(indexPath);
