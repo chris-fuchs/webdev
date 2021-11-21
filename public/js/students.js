7f51a1fe13dc44557574db1e666fb49a7c8c0b8e
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-21 15:00:33 +0100

    Quickfix

--- public/js/students.js
+++ public/js/students.js
@@ -102,7 +102,7 @@ const students =
     {"ID":"68845","FirstName":"Cross","LastName":"Raspel","Gender":"2","DOB":"2003/11/21","Department":"Finance","EMailID":"craspel2r@ca.gov","JoiningDate":"2020/09/10"}
 ]
 
-var students
+
 
 
 
