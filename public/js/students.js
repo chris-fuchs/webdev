a183bca24c02f12382fb4544282adaeaeb8ee0dd
Christian Fuchs <chris.fuchs88@gmail.com>
2021-11-21 14:59:27 +0100

    use javascript object instead of json

--- public/js/students.js
+++ public/js/students.js
@@ -1,4 +1,4 @@
-/*const students = 
+const students = 
     [
     {"ID":"29724","FirstName":"Kippy","LastName":"Shallo","Gender":"2","DOB":"1991/10/22","Department":"IT","EMailID":"kshallo0@europa.eu","JoiningDate":"2017/03/03"},
     {"ID":"79488","FirstName":"Nerte","LastName":"Davet","Gender":"2","DOB":"2003/06/27","Department":"Art","EMailID":"ndavet1@vinaora.com","JoiningDate":"2018/06/30"},
@@ -100,7 +100,7 @@
     {"ID":"13968","FirstName":"Mab","LastName":"Rivelin","Gender":"0","DOB":"2003/07/16","Department":"Finance","EMailID":"mrivelin2p@npr.org","JoiningDate":"2018/05/04"},
     {"ID":"62215","FirstName":"Lelia","LastName":"Gasnell","Gender":"2","DOB":"1992/04/15","Department":"Finance","EMailID":"lgasnell2q@twitpic.com","JoiningDate":"2017/06/02"},
     {"ID":"68845","FirstName":"Cross","LastName":"Raspel","Gender":"2","DOB":"2003/11/21","Department":"Finance","EMailID":"craspel2r@ca.gov","JoiningDate":"2020/09/10"}
-]*/
+]
 
 var students
 
@@ -112,13 +112,13 @@ var btn;
 var span;
 
 window.onload = function() {
-
+/*
     const myRequest = new Request('students.json');
     fetch(myRequest)
     .then(response => response.json().then(data => {
         students = data
     }))
-    .catch(console.error);
+    .catch(console.error);*/
 
 
     modal = document.getElementById("modal");
